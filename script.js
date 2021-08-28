7bbbb8af47f25b5ae0c255732e17b97e68993f97
Roberto T <77257812+robertoTorres10@users.noreply.github.com>
2021-08-28 14:18:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+
+// Screen Selection
+// let navAnchors = document.querySelectorAll("div#screen a");
+// document.querySelector("#home").style.display = "block";
+// navAnchors.forEach( (anchor) => {
+//   anchor.addEventListener("click", (event) => {
+//     document.querySelectorAll(".screen").forEach((screen) => {
+//       screen.style.display = "none";
+//     });
+//     let screen = event.target.getAttribute("data-screen");
+//     let targetScreen = document.querySelector("#" + screen);
+//     targetScreen.style.display = "block";
+//   })
+// })
